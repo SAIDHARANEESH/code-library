f0f857dc6c7ac52e931fc9e45a1990b09b52c178
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2020-10-10 00:00:59 +0530

    hello problem solved

--- Data Structures/FenwickTree.cpp
+++ Data Structures/FenwickTree.cpp
@@ -2,7 +2,10 @@
 struct FenwickTree {
 	vector<int> bit;
 	int n;
-
+	/*
+		n stores the size of fenwick tree
+		bit stores the results of the fenwick tree
+	*/
 	FenwickTree(int n) {
 		this->n = n;
 		bit.assign(n, 0);
@@ -13,7 +16,7 @@ struct FenwickTree {
 			add(i, a[i]);
 		}
 	}
-
+	//query to find sum between the range 1 and r
 	int sum(int r) {
 		int res = 0;
 		for (; r >= 0; r = (r & (r + 1)) - 1)
@@ -21,10 +24,11 @@ struct FenwickTree {
 		return res;
 	}
 	
+	//query to find sum between the range l and r
 	int sum(int l, int r) {
 		return sum(r) - sum(l - 1);
 	}
-
+	// building of fenwick tree
 	void add(int index, int delta) {
 		for (; index < n; index = index | (index + 1)) {
 			bit[index] += delta;
