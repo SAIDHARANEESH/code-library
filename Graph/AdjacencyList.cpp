d03191f44afcd79ad7b3b9787db8213a82719541
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2020-10-10 00:06:50 +0530

    adjacency list for edges and cost

---
+++ Graph/AdjacencyList.cpp
@@ -0,0 +1,15 @@
+
+// for edge
+vector<vector<int>> adj;
+
+void build(int n) {
+	adj.resize(n);
+}
+
+
+// for edge and cost
+vector<int, pair<int, int>> adj;
+
+void build(int n) {
+	adj.resize(n);
+}
