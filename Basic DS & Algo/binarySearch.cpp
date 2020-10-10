76703b67453a21a1efc9d6859539ec819aa66fb1
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2020-10-10 06:29:48 +0530

    search algorithm in logn time

---
+++ Basic DS & Algo/binarySearch.cpp
@@ -0,0 +1,13 @@
+void binarySearch(int a[], int left, int right, int number) {
+    if (left < right) {
+        int mid = (left + right) >> 1;
+        if (a[mid] == number) {
+            cout << "Number found";
+        }
+        if (number < mid)
+            binarySearch(a, left, mid);
+        else
+            binarySearch(a, mid + 1, right);
+    }
+    cout << "Number not found";
+}
