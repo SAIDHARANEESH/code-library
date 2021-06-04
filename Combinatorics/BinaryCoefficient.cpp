6a558c2bc2155491af9b1a39fe74529654bc0829
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2021-06-04 19:48:13 +0530

    binomial updated

--- Combinatorics/BinaryCoefficient.cpp
+++ Combinatorics/BinaryCoefficient.cpp
@@ -1,9 +1,3 @@
-int BinaryCoefficinet(int n, int k) {
-	int res = 1;
-	for (int i = n - k + 1; i <= n; i++) {
-		res *= i;
-	}
-	for (int i = 2; i <= k; i++) {
-		res /= i;
-	}
+for (int i = 0; i < n; i++) {
+	cout << a[i];
 }
