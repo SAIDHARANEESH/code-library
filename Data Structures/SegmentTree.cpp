50ee4da46501580552686193ba12f22f1c75375c
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2021-06-04 19:04:22 +0530

    updated segment tree

--- Data Structures/SegmentTree.cpp
+++ Data Structures/SegmentTree.cpp
@@ -1,41 +1,25 @@
-
-
-
-
-void build(int a[], int left, int right, int v) {
-	if (left == right) {
-		t[v] = a[left];
-	}
-	else {
-		int mid = (left + right) >> 1;
-		build(a, left, mid, 2 * v + 1);
-		build(a, mid + 1, right, 2 * v + 2);
+void build(int v, int l, int r, int a[]) {
+	if (l == r) { 
+		t[v] = a[l];
+	} else {
+		int mid = (l + r) >> 1;
+		build(2 * v + 1, l, mid, a);
+		build(2 * v + 2, mid + 1, r, a);
+		t[v] = t[2 * v + 1] + t[2 * v + 2];
 	}
 }
 
-void update(int v, int left, int right, int pos, int value) {
-	if (left == right) {
-		t[v] = value;
-	}
-	else {
-		int mid = (left + right) >> 1;
+void update(int v, int l, int r, int pos, int val) {
+	if (l == r) {
+		t[v] = val;
+	} else {
+		int mid = (l + r) >> 1;
 		if (pos <= mid) {
-			update(2 * v + 1, left, mid, pos, value);
-		}
-		else {
-			update(2 * v + 2, mid + 1, right, pos, value);
+			update(2 * v + 1, l, mid, pos, val);
+		} else {
+			update(2 * v + 2, mid + 1, r, pos, val);
 		}
 		t[v] = t[2 * v + 1] + t[2 * v + 2];
 	}
 }
 
-int sum(int v, int left, int right, int l, int r) {
-	if (l > r) {
-		return 0;
-	}
-	if (l == left && r == right) {
-		return t[v];
-	}
-	int mid = (left + right) >> 1;
-	return sum(2 * v + 1, left, mid, l, min(r, mid)) + sum(2 * v + 2, mid + 1, right, max(l, mid + 1), r);
-}
