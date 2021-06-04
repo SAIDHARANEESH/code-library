c00b08d118daead237a29c2f7b38eeebbfce6d71
SAIDHARANEESH <gurramsaidharaneesh264@gmail.com>
2021-06-04 19:09:28 +0530

    updated insertion sort

--- Basic DS & Algo/InsertionSort.cpp
+++ Basic DS & Algo/InsertionSort.cpp
@@ -1,12 +1,3 @@
-void insertionSort(int a[], int len) {
-	int i, key, j
-	for (i = 1; i < len; i++) {
-		key = a[i];
-		j = i - 1;
-		while (j >= 0 && a[j] > key) {
-			a[j + 1] = a[j];
-			j = j - 1;
-		}
-		a[j + 1] = key;
-	}
+void insert() {
+	return a * b;
 }
